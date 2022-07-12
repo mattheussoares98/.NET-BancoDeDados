b786dece22f66db1897cf3d19dc7fb5e59b5f3c2
Mattheus <mattheussbarbosa@hotmail.com>
2022-07-12 17:01:57 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace BancoDeDados {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonConectar = new System.Windows.Forms.Button();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.buttonCriarTabela = new System.Windows.Forms.Button();
+            this.buttonInserir = new System.Windows.Forms.Button();
+            this.buttonPesquisar = new System.Windows.Forms.Button();
+            this.buttonExcluir = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nome,
+            this.Email});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 254);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(647, 286);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "NOME";
+            this.Nome.Name = "Nome";
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "EMAIL";
+            this.Email.Name = "Email";
+            // 
+            // buttonConectar
+            // 
+            this.buttonConectar.Location = new System.Drawing.Point(671, 242);
+            this.buttonConectar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonConectar.Name = "buttonConectar";
+            this.buttonConectar.Size = new System.Drawing.Size(236, 43);
+            this.buttonConectar.TabIndex = 1;
+            this.buttonConectar.Text = "Conectar";
+            this.buttonConectar.UseVisualStyleBackColor = true;
+            this.buttonConectar.Click += new System.EventHandler(this.buttonConectar_Click);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.Location = new System.Drawing.Point(17, 16);
+            this.labelResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(877, 101);
+            this.labelResultado.TabIndex = 2;
+            this.labelResultado.Text = "Resultado";
+            // 
+            // labelNome
+            // 
+            this.labelNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNome.Location = new System.Drawing.Point(17, 127);
+            this.labelNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(203, 49);
+            this.labelNome.TabIndex = 3;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEmail.Location = new System.Drawing.Point(17, 188);
+            this.labelEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(203, 49);
+            this.labelEmail.TabIndex = 4;
+            this.labelEmail.Text = "Email";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNome.Location = new System.Drawing.Point(165, 129);
+            this.textBoxNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(495, 38);
+            this.textBoxNome.TabIndex = 5;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(165, 188);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(496, 38);
+            this.textBoxEmail.TabIndex = 6;
+            // 
+            // buttonCriarTabela
+            // 
+            this.buttonCriarTabela.Location = new System.Drawing.Point(669, 293);
+            this.buttonCriarTabela.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCriarTabela.Name = "buttonCriarTabela";
+            this.buttonCriarTabela.Size = new System.Drawing.Size(236, 43);
+            this.buttonCriarTabela.TabIndex = 7;
+            this.buttonCriarTabela.Text = "Criar tabela";
+            this.buttonCriarTabela.UseVisualStyleBackColor = true;
+            // 
+            // buttonInserir
+            // 
+            this.buttonInserir.Location = new System.Drawing.Point(671, 343);
+            this.buttonInserir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonInserir.Name = "buttonInserir";
+            this.buttonInserir.Size = new System.Drawing.Size(236, 43);
+            this.buttonInserir.TabIndex = 8;
+            this.buttonInserir.Text = "Inserir";
+            this.buttonInserir.UseVisualStyleBackColor = true;
+            // 
+            // buttonPesquisar
+            // 
+            this.buttonPesquisar.Location = new System.Drawing.Point(671, 394);
+            this.buttonPesquisar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonPesquisar.Name = "buttonPesquisar";
+            this.buttonPesquisar.Size = new System.Drawing.Size(236, 43);
+            this.buttonPesquisar.TabIndex = 9;
+            this.buttonPesquisar.Text = "Pesquisar";
+            this.buttonPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // buttonExcluir
+            // 
+            this.buttonExcluir.Location = new System.Drawing.Point(671, 444);
+            this.buttonExcluir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonExcluir.Name = "buttonExcluir";
+            this.buttonExcluir.Size = new System.Drawing.Size(236, 43);
+            this.buttonExcluir.TabIndex = 10;
+            this.buttonExcluir.Text = "Excluir";
+            this.buttonExcluir.UseVisualStyleBackColor = true;
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(671, 495);
+            this.buttonEditar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(236, 43);
+            this.buttonEditar.TabIndex = 11;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonExcluir);
+            this.Controls.Add(this.buttonPesquisar);
+            this.Controls.Add(this.buttonInserir);
+            this.Controls.Add(this.buttonCriarTabela);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.buttonConectar);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.Button buttonConectar;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Button buttonCriarTabela;
+        private System.Windows.Forms.Button buttonInserir;
+        private System.Windows.Forms.Button buttonPesquisar;
+        private System.Windows.Forms.Button buttonExcluir;
+        private System.Windows.Forms.Button buttonEditar;
+    }
+}
+
